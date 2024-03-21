2c05cd19c4897c1239a8f1e17c68ac503b867c9c
Jay-Lokhande <jaylokhande2711@gmail.com>
2024-03-21 19:28:47 +0530

    dashboard changes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Page = () => {
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
+    if (!user || !user) redirect('/auth-callback?origin=dashboard')
+
+    return <div>hi</div>
+}
+export default Page;
