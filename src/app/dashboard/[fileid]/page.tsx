d02e8d2dfaaab619e98fd564eeb516a3a72c60ab
Jay-Lokhande <jaylokhande2711@gmail.com>
2024-03-22 14:39:19 +0530

    dashboard updated

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,4 @@
+// import ChatWrapper from '@/components/chat/ChatWrapper'
 import ChatWrapper from '@/components/ChatWrapper'
 import RenderedPDF from '@/components/RenderedPDF'
 import { db } from '@/db'
@@ -20,16 +21,16 @@ const Page = async ({ params }: PageProps) => {
   if (!user || !user.id)
     redirect(`/auth-callback?origin=dashboard/${fileid}`)
 
-  const file = db.file.findFirst({
+  const file = await db.file.findFirst({
     where: {
       id: fileid,
       userId: user.id,
     },
   })
-
+  console.log(file?.url)
   if (!file) notFound()
 
-//   const plan = await getUserSubscriptionPlan()
+  // const plan = await getUserSubscriptionPlan()
 
   return (
     <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
@@ -38,12 +39,13 @@ const Page = async ({ params }: PageProps) => {
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
             {/* Main area */}
-            <RenderedPDF />
+            
+            <RenderedPDF url={file?.url} />
           </div>
         </div>
 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-          <ChatWrapper  />
+           <ChatWrapper/>
         </div>
       </div>
     </div>
