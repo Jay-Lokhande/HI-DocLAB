097dba8a7935f2344e4789fe83ec4fa51b2a22bc
Jay Lokhande <91283261+Jay-Lokhande@users.noreply.github.com>
2024-04-12 16:45:26 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <MaxWidthWrapper className="mb-12 mt=28 sm:mt-40 flex flex-col items-center justify-center text-center">
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
         <p className="text-sm font-semibold text-gray-700">
-          hadlib is now public
+          <em>HI-D</em>oc<em>LAB</em> is now public
         </p>
       </div>
       <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
@@ -16,7 +16,7 @@ export default function Home() {
       Interact with your   <span className="text-purple-600">documents</span> instantly through chat.
       </h1>
       <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-      Hadlib enables you to engage in conversations with any PDF document. Just upload your file, and you can start asking questions immediately.      </p>
+      HI-DocLAB enables you to engage in conversations with any PDF document. Just upload your file, and you can start asking questions immediately.      </p>
       <Link className={buttonVariants({
         size: 'lg',
         className: 'mt-5',
