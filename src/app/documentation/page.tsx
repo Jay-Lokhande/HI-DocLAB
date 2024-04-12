40c9a41f47860fe145d62711ede67caab7093d45
Jay Lokhande <91283261+Jay-Lokhande@users.noreply.github.com>
2024-04-12 19:38:59 +0530

    Update page.tsx

--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -1,1 +1,102 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import UpgradeButton from '@/components/UpgradeButton'
+import { buttonVariants } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import { PLANS } from '@/config/stripe'
+import { cn } from '@/lib/utils'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+  ArrowRight,
+  Check,
+  HelpCircle,
+  Minus,
+} from 'lucide-react'
+import Link from 'next/link'
 
+const Page = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  const pricingItems = [
+    {
+      plan: 'Free',
+      tagline: 'For small side projects.',
+      quota: 10,
+      features: [
+        {
+          text: '5 pages per PDF',
+          footnote:
+            'The maximum amount of pages per PDF-file.',
+        },
+        {
+          text: '4MB file size limit',
+          footnote:
+            'The maximum file size of a single PDF file.',
+        },
+        {
+          text: 'Mobile-friendly interface',
+        },
+        {
+          text: 'Higher-quality responses',
+          footnote:
+            'Better algorithmic responses for enhanced content quality',
+          negative: true,
+        },
+        {
+          text: 'Priority support',
+          negative: true,
+        },
+      ],
+    },
+    {
+      plan: 'Pro',
+      tagline: 'For larger projects with higher needs.',
+      quota: PLANS.find((p) => p.slug === 'pro')!.quota,
+      features: [
+        {
+          text: '25 pages per PDF',
+          footnote:
+            'The maximum amount of pages per PDF-file.',
+        },
+        {
+          text: '16MB file size limit',
+          footnote:
+            'The maximum file size of a single PDF file.',
+        },
+        {
+          text: 'Mobile-friendly interface',
+        },
+        {
+          text: 'Higher-quality responses',
+          footnote:
+            'Better algorithmic responses for enhanced content quality',
+        },
+        {
+          text: 'Priority support',
+        },
+      ],
+    },
+  ]
+
+  return (
+    <>
+      <MaxWidthWrapper className='mb-8 mt-24 text-center max-w-5xl'>
+        <div className='mx-auto mb-10 sm:max-w-lg'>
+          <h1 className='text-6xl font-bold sm:text-7xl'>
+            Documentation
+          </h1>
+          <p className='mt-5 text-gray-600 sm:text-lg'>
+            This is the documentation page for HI-DocLAB.
+          </p>
+        </div>
+      </MaxWidthWrapper>
+    </>
+  )
+}
+
+export default Page
