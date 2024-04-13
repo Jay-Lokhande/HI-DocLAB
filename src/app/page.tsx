c69ba2220a1e4fd68994d19a4dd981622a9f6da6
Jay Lokhande <91283261+Jay-Lokhande@users.noreply.github.com>
2024-04-13 16:26:50 +0530

    Update page.tsx (added links and logo)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,8 +115,8 @@ export default function Home() {
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <a href="https://hidoclab.vercel.app/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <img src="/logo.png" className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HI-DocLAB</span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -135,7 +135,7 @@ export default function Home() {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">HI-DocLAB™</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://hidoclab.vercel.app/" className="hover:underline">HI-DocLAB™</a>. All Rights Reserved.</span>
       </div>
     </footer>
 
